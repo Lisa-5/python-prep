c42ad9d88171f45d90facacc28a1f8364c0f0584
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-03-22 10:36:30 -0500

    wip

---
+++ collection_and_iteration/examples.py
@@ -0,0 +1,12 @@
+# letters = {'a', 'b', 'c', 'b', 'a'}
+# letters.add('d')
+# print(letters)
+
+# frozen_letters = frozenset(letters)
+# frozen_letters.add('e')
+
+# letters.add('c')
+# print(letters)
+
+numbers = {1, 2, 37, 4, 5}
+print(numbers)
