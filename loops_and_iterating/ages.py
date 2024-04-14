7e5e5262cc0f40c443f92ea218f194c6fef4acb7
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-04-14 16:26:47 -0500

    corrections

--- loops_and_iterating/ages.py
+++ loops_and_iterating/ages.py
@@ -7,15 +7,15 @@
 # In 40 years, you will be 67 years old.
 
 age = int(input('How old are you? '))
-print()
-
 print(f'You are {age} years old.')
+print()
+# future_years = [10, 20, 30, 40] #range(10, 50, 10)
 
-future_years = [10, 20, 30, 40] #range(10, 50, 10)
-
-for year in future_years:
-    print(f'In {year} years, you will be {age + year} years old.')
-
+# for year in future_years:
+#     print(f'In {year} years, you will be {age + year} years old.')
+for future_year in range(10, 50, 10):
+    print(f'In {future_year} years, you will be '
+          f'{age + future_year} years old.')
 
 
 
